dcf7715a5a358644854225223b0a8783242bc455
Liam Maeder <liam.maeder@gmail.com>
2022-06-16 00:36:02 +0200

    committing corrections and current version of page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import NavBar from "./components/NavBar";
-// import Hero from "./components/Hero";
+import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
-    return (
-        <div className="container">
-            <NavBar />
-            <Card
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life lessons with Katie Zaferes"
-                price={136}
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
             />
+        )
+    })        
+    
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 } 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,15 @@
 export default function Card(props) {
     return (
-            <div className="card">
-                <img src={require(`../images/${props.img}`)} alt={props.name } className="card--image" />
-                <div className="card--stats">
-                    <img src={require("../images/star.png")} alt="Star" className="card--star" />
-                    <span>{props.rating}</span>
-                    <span className="grey">({props.reviewCount})&nbsp;•&nbsp;</span>
-                    <span className="grey">{props.country}</span>
-                </div>
-                <div className="card--content">
-                    <p>{props.title}</p>
-                    <p><span className="bold">From ${props.price}</span> / person</p>
-                </div>
+        <div className="card">
+            <img src={require(`../images/${props.img}`)} className="card--image" />
+            <div className="card--stats">
+                <img src={require("../images/star.png")} className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount})&nbsp;•&nbsp;</span>
+                <span className="gray">{props.location}</span>
             </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+        </div>
     )
 }
